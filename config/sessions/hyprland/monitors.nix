3711a836c2514419e309766039b79b8ef8062340
Moukhtar Morsy <89392836+moukhtar22@users.noreply.github.com>
2026-03-06 19:52:31 +0300

    two screen

--- config/sessions/hyprland/monitors.nix
+++ config/sessions/hyprland/monitors.nix
@@ -3,7 +3,8 @@
 {
   wayland.windowManager.hyprland.settings = {
     monitor = [
-      "eDP-1, 1920x1080@144, 0x0, 1.0"
+      "DP-2, 1920x1080@75, 0x0, 1.0"
+      "DP-3, 1920x1080@75, 1920x0, 1.0"
     ];
   }; 
 }
